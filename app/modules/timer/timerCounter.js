27a8f9617ddcc177834bb5abab341d664bff27b2
benas-juskus <benas.juskus@stud.ktmc.lt>
2023-11-03 13:13:16 +0200

    Removed more lines

--- app/modules/timer/timerCounter.js
+++ app/modules/timer/timerCounter.js
@@ -1,10 +1,10 @@
 import { timerDisplay } from "./timerDisplay.js";
 
-export function timerCounter(display){
+export function timerCounter(){
     let min = 0;
     let sec = 1;
     let timer = setInterval(() => {
-        timerDisplay(min, sec, display);
+        timerDisplay(min, sec);
         sec++;
         if (sec > 59) {
             min++;
