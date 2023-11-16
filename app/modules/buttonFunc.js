3df8a87e2df2329d89201f4ddcf90d106eb282ae
KestutisVaitiekunas <kestutis.vaitiekunas@stud.ktmc.lt>
2023-11-16 13:02:48 +0200

    moved functions to sepatate modules. added documentation

--- app/modules/buttonFunc.js
+++ app/modules/buttonFunc.js
@@ -1,11 +1,17 @@
 import {activeFieldCheck, activeSameCheck, activeZoneCheck} from "./activeField.js"
-
-function openStartPanel(){// when "New game" button is pressed opens a start new game panel 
+/**
+ *  when "New game" button is pressed opens a start new game panel 
+ */
+function openStartPanel(){
    document.getElementsByClassName("new-game")[0].addEventListener("click", function(){
    document.getElementById("start_new").style.visibility = "visible" // makes element visable
    });
 };
-function dificultyClosePanel(){// when dificulty button is pressed opens a start new game panel 
+/**
+ *  when dificulty button is pressed opens a start new game panel 
+ * and cliers the grid of active fields and zones 
+ */
+function dificultyClosePanel(){//
    let dif_buttons = document.querySelectorAll(".dif");
    dif_buttons.forEach(button => {
       button.addEventListener("click", () => {
@@ -13,8 +19,7 @@ function dificultyClosePanel(){// when dificulty button is pressed opens a start
          activeSameCheck();
          activeZoneCheck();
          document.getElementById("start_new").style.visibility = "hidden";
-
-      }); // makes element visable)
+      });
    });
 };
 function startGame(){ // closes start new game panel (reserved to  "start game" for future) 
@@ -23,12 +28,21 @@ function startGame(){ // closes start new game panel (reserved to  "start game" 
    visibilityChange("winner_new_game", "start_new", "visible");
    visibilityChange("start_new_game", "start_new", "visible");
 };
+/**
+ * ads event listeners to hide elements
+ */
 function cancelStartGame(){// closes start new game panel, game over panel,
    visibilityChange("start_game_cancel", "start_new", "hidden");
    visibilityChange("game_over_cancel", "game_over", "hidden");
    visibilityChange("winner_cancel", "winner", "hidden");
 };
-
+/**
+ * adds event listener on element to change its visibility status 
+ * @param {string} button_id element id
+ * @param {string} element_id element id
+ * @param {string} visibility style name
+ * @example visibilityChange("winner_cancel", "winner", "hidden")
+ */
 function visibilityChange(button_id, element_id, visibility){
    document.getElementById(button_id).addEventListener("click", function(){
       document.getElementById(element_id).style.visibility = visibility;
